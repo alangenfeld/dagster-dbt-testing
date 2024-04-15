a23b31a14a55b1e1072d323bcad3e939137c8f80
Alex Langenfeld <alexlangenfeld@gmail.com>
2024-04-15 11:25:54 -0500

    testing action v1.38 (#3)

--- dagster_dbt_testing/dagster_dbt_testing/assets.py
+++ dagster_dbt_testing/dagster_dbt_testing/assets.py
@@ -6,4 +6,5 @@ from .project import dagster_dbt_testing_project
 
 @dbt_assets(manifest=dagster_dbt_testing_project.manifest_path)
 def jaffle_shop_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+    context.log.info(f"defer args: {dbt.get_defer_args()}")
     yield from dbt.cli(["build"], context=context).stream()
